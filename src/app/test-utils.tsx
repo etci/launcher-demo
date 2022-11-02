afc06ca2a9faa2f5a232bdd46004f4d6202ff048
oke <kutayetci@gmail.com>
2022-11-02 16:51:53 +0100

    Make renderWithRedux api similar to rtlRender

--- src/app/test-utils.tsx
+++ src/app/test-utils.tsx
@@ -3,15 +3,11 @@ import { render as rtlRender, RenderOptions } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { store as appStore } from "./store";
 
-interface RenderWithReduxParams {
-  ui: React.ReactElement;
-  options: RenderOptions & { store?: Store };
-}
-
-function renderWithRedux({
-  ui,
-  options: { store = appStore, ...rest } = {},
-}: RenderWithReduxParams): ReturnType<typeof rtlRender> {
+type Options = RenderOptions & { store?: Store };
+function renderWithRedux(
+  ui: React.ReactElement,
+  { store = appStore, ...rest }: Options = {},
+): ReturnType<typeof rtlRender> {
   function Wrapper({ children }: { children: React.ReactElement }) {
     return <Provider store={store}>{children}</Provider>;
   }
